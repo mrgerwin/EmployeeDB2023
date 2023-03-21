36dc218d530648d9175a1bf6200ee2d5e95b4a80
mrgerwin <jgerwin@otsegoknights.org>
2023-03-21 11:46:17 -0400

    DataLists

--- EmployeeDB2023.py
+++ EmployeeDB2023.py
@@ -27,4 +27,13 @@ nextButton = PushButton(buttonBox, text="Next", grid=[0,0])
 saveButton = PushButton(buttonBox, text="Save", grid=[1,0])
 newButton = PushButton(buttonBox, text="New", grid=[2,0])
 
+
+#["Brewster.jpeg", "Danko.jpeg", "Dzierwa.jpeg", "Ferdig.jpeg", "Krabill.jpeg", "Lambert.jpeg", "Oshea.jpeg", "Roberts.png", "Ruckstuhl.jpeg"]
+#["Mike", "Julie", "Matt", "Tom", "Nick", "Natalie", "Kevin", "Betsy", "Brian"]
+#["Brewster", "Danko", "Dzierwa", "Ferdig", "Krabill", "Lambert", "Oshea", "Roberts", "Ruckstuhl"]
+#["3562", "1324", "2049", "3420", "9463", "5603", "3420", "2304", "6707"]
+#[80000,82475, 74300, 126000, 52600, 65230, 267000, 62000, 54320]
+#["Science Teacher", "Social Studies Teacher", "Math Teacher", "Assistant Principal", "Math Teacher", "Spanish Teacher", "Superintendent", "Secretary", "Deputy"]
+#[23, 33, 26, 18, 2, 9, 15, 16, 6]
+
 app.display()
