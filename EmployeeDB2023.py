591e20497b51eb4e48aa4a65d6c909ed64acdd97
mrgerwin <jgerwin@otsegoknights.org>
2023-03-22 13:43:44 -0400

    After first half

--- EmployeeDB2023.py
+++ EmployeeDB2023.py
@@ -1,5 +1,39 @@
 from guizero import *
 
+def pressNext():
+    global index
+    
+    index += 1
+    
+    if index == len(picList):
+        index = 0
+        
+    pic.value = picList[index]
+    firstNameText.value = fnameList[index]
+    lastNameText.value =lnameList[index]
+    employeeNumText.value =enList[index]
+    salaryText.value = salList[index]
+    jobText.value = jobList[index]
+    yearsText.value = yearsList[index]
+
+def pressSave():
+    f = open("data.txt", "w")
+    for i in range(len(picList)):
+        f.write(picList[i]+","+ fnameList[i]+ ","+ lnameList[i] + ","+enList[i] + "," + str(salList[i])+","+ jobList[i] + "," + str(yearsList[i])+"\n" )
+    f.close()
+    
+def pressNew():
+    global index
+    
+    index = 0
+    pic.value = "./Default.png"
+    firstNameText.value = ""
+    lastNameText.value =""
+    employeeNumText.value =""
+    salaryText.value = ""
+    jobText.value = ""
+    yearsText.value = ""
+
 app = App(title="Employee Database", width = 450, height = 600, layout="grid")
 
 pic = Picture(app, image="./Default.png", grid=[0,0, 2, 1])
@@ -23,17 +57,18 @@ yearsBox= TitleBox(app, "Years Experience", grid=[1,3])
 yearsText= TextBox(yearsBox, width=15)
 
 buttonBox = TitleBox(app, "Buttons", layout="grid", grid=[0,4,2,1])
-nextButton = PushButton(buttonBox, text="Next", grid=[0,0])
-saveButton = PushButton(buttonBox, text="Save", grid=[1,0])
-newButton = PushButton(buttonBox, text="New", grid=[2,0])
+nextButton = PushButton(buttonBox, text="Next", grid=[0,0], command = pressNext)
+saveButton = PushButton(buttonBox, text="Save", grid=[1,0], command = pressSave)
+newButton = PushButton(buttonBox, text="New", grid=[2,0], command = pressNew)
 
+index = 0
 
-#["Brewster.jpeg", "Danko.jpeg", "Dzierwa.jpeg", "Ferdig.jpeg", "Krabill.jpeg", "Lambert.jpeg", "Oshea.jpeg", "Roberts.png", "Ruckstuhl.jpeg"]
-#["Mike", "Julie", "Matt", "Tom", "Nick", "Natalie", "Kevin", "Betsy", "Brian"]
-#["Brewster", "Danko", "Dzierwa", "Ferdig", "Krabill", "Lambert", "Oshea", "Roberts", "Ruckstuhl"]
-#["3562", "1324", "2049", "3420", "9463", "5603", "3420", "2304", "6707"]
-#[80000,82475, 74300, 126000, 52600, 65230, 267000, 62000, 54320]
-#["Science Teacher", "Social Studies Teacher", "Math Teacher", "Assistant Principal", "Math Teacher", "Spanish Teacher", "Superintendent", "Secretary", "Deputy"]
-#[23, 33, 26, 18, 2, 9, 15, 16, 6]
+picList=["Brewster.jpeg", "Danko.jpeg", "Dzierwa.jpeg", "Ferdig.jpeg", "Krabill.jpeg", "Lambert.jpeg", "Oshea.jpeg", "Roberts.png", "Ruckstuhl.jpeg"]
+fnameList=["Mike", "Julie", "Matt", "Tom", "Nick", "Natalie", "Kevin", "Betsy", "Brian"]
+lnameList=["Brewster", "Danko", "Dzierwa", "Ferdig", "Krabill", "Lambert", "Oshea", "Roberts", "Ruckstuhl"]
+enList = ["3562", "1324", "2049", "3420", "9463", "5603", "3420", "2304", "6707"]
+salList = [80000,82475, 74300, 126000, 52600, 65230, 267000, 62000, 54320]
+jobList= ["Science Teacher", "Social Studies Teacher", "Math Teacher", "Assistant Principal", "Math Teacher", "Spanish Teacher", "Superintendent", "Secretary", "Deputy"]
+yearsList =[23, 33, 26, 18, 2, 9, 15, 16, 6]
 
 app.display()
